b0f3505756492f55ce6977e75cfb3db45f5452ff
Kingsley Odim <kingodim@outlook.com>
2024-11-09 10:43:06 +0100

    second commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,10 +7,10 @@ import { ModeToggle } from "@/app/components/ModeToggle";
 const Header = () => {
   return (
     <header className="w-full border-b border-border bg-background">
-      <div className="container flex h-20 items-center justify-around">
+      <div className="container flex h-20 items-center justify-between px-4 md:px-8">
         <div className="flex items-center gap-2">
           {/* <Image src="/debug.png" alt="Logo" width={48} height={48} /> */}
-          <span className="text-xl font-bold text-foreground">Loveworld Programs Calendar</span>
+          <span className="text-lg sm:text-xl font-bold text-foreground">Loveworld Programs Calendar</span>
         </div>
         <div>
           <ModeToggle />
