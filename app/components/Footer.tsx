65ea7b47e44e13c698856adb2603493d6127050b
Kingsley Odim <kingodim@outlook.com>
2024-11-09 10:27:20 +0100

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="w-full py-4 text-center text-sm text-gray-600">
+      Powered by Debug
+    </div>
+  );
+}
+
+export default Footer;
---
+++ app/components/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { ModeToggle } from "@/app/components/ModeToggle";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b border-border bg-background">
+      <div className="container flex h-20 items-center justify-around">
+        <div className="flex items-center gap-2">
+          {/* <Image src="/debug.png" alt="Logo" width={48} height={48} /> */}
+          <span className="text-xl font-bold text-foreground">Loveworld Programs Calendar</span>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
